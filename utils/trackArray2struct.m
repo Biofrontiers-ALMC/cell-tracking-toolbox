9f77fbd84bc156f696da4a5534acd7f51ed71ee8
Jian Tay <jwtay.physics@gmail.com>
2019-06-14 16:37:28 -0600

    WIP: Adding utility to convert track array to struct

---
+++ utils/trackArray2struct.m
@@ -0,0 +1,27 @@
+function structOut = trackArray2struct(trackarray)
+
+if ~isa(trackarray, 'TrackDataArray')
+    error('trackArray2struct:NotTrackDataArray', ...
+        'Expected input to be a TrackDataArray object but it is a %s object instead.', ...
+        class(trackarray));
+end
+
+structOut = struct;
+
+%Import each track as a new struct
+for iTrack = 1:trackarray.NumTracks
+    
+    ct = getTrack(trackarray, iTrack);
+    
+    newIdx = numel(structOut) + 1;
+    structOut(newIdx).FirstFrame = ct.FirstFrame;
+    structOut(newIdx).LastFrame = ct.LastFrame;
+    
+    for iP = 1:numel(ct.TrackDataProps)
+        structOut(newIdx).(ct.TrackDataProps{iP}) = ...
+            getData(ct, ct.TrackDataProps{iP});
+    end
+    
+end
+
+end
